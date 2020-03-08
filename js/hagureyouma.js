cd7ad7ec7e533e131cd6ff1971cfac69434c75d7
hagureyouma <61862204+hagureyouma@users.noreply.github.com>
2020-03-08 19:03:08 +0900

    Update hagureyouma.js

--- js/hagureyouma.js
+++ js/hagureyouma.js
@@ -2,7 +2,7 @@
 
 var insertHTML = function (htmlString, className) {
     var x;
-    Array.from(document.getElementsByClassName(className)).forEach(x =>  x.insertAdjacentHTML('beforeend', htmlString));
+    Array.from(document.getElementsByClassName(className)).forEach(x => x.insertAdjacentHTML('beforeend', htmlString));
 }
 
 var return_home = function () {
@@ -15,12 +15,14 @@ var return_history = function () {
     insertHTML(s, "return-history");
 }
 
-
-document.head.insertAdjacentHTML('beforeend', '<link href="/css/hagureyouma.css" rel="stylesheet" type="text/css">');
-document.head.insertAdjacentHTML('beforeend', '<link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">');
-document.head.insertAdjacentHTML('beforeend', '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">');
+var load_resources = function () {
+    document.head.insertAdjacentHTML('beforeend', '<link href="/css/hagureyouma.css" rel="stylesheet" type="text/css">');
+    document.head.insertAdjacentHTML('beforeend', '<link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">');
+    document.head.insertAdjacentHTML('beforeend', '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">');
+}
 
 document.addEventListener('DOMContentLoaded', function () {
+    load_resources();
     return_home();
     return_history();
 });
